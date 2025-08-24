64f51bf87e75c4cde8e5f3e95badad1dc7479e3c
aboy4321 <awboyce630@gmail.com>
2025-08-24 01:44:55 -0400

    Added Linear Layer

---
+++ linear.h
@@ -0,0 +1,60 @@
+#include <vector>
+#include <cstdlib>
+using namespace std;
+
+class Linear{
+    private:
+        int input_dim, output_dim;
+        vector<double> input_vals, output_vals;
+        double eta;
+        vector<vector<double>> weights;
+    public:
+        Linear(){}
+        Linear(int input_size, int output_size, double learning_rate);
+        vector<double> forward(const vector<double> &input);
+        vector<double> backprop(const vector<double> &grad);
+};
+Linear::Linear(int input_size, int output_size, double learning_rate){
+    output_dim = output_size;
+    input_dim = input_size;
+    eta = learning_rate;
+    
+    for(int o = 0; o < output_size; o++){
+        weights.push_back(vector<double>());
+        for(int i = 0; i < input_size + 1; i++){
+            weights.back().push_back((double)rand() / RAND_MAX);
+        }
+    }
+}
+vector<double> Linear::forward(const vector<double> &input){
+    output_vals = vector<double>();
+    input_vals = input;
+
+    for(int = 0; o < input_dim; o++){
+        double sum = 0.0;
+        for(int w = 0; w < input_dim; w++){
+            sum += weights[o][w] * input[w];
+        }
+        sum += weights[o][input_dim];
+        output_vals.push_back(sum);
+    }
+    return output_vals;
+}
+vector<double> Linear::backprop(const vector<double> &grad){
+    vector<double> prev_grad;
+
+    for(int i = 0; i < input_dim; i++){
+        double g = 0.0;
+        for(int o = 0; o < output_dim; o++){
+            g += (grad[o] * weights[o][i]);
+        }
+        prev_grad.push_back(g);
+    }
+    for(int o = 0; o < output_dim; o++){
+        for(int i = 0; i < input_dim; i++){
+            weights[o][i] -= (eta * grad[o] * input_vals[i]);
+        }
+        weights[o][input_dim] -= eta * grad[o];
+    }
+    return prev_grad;
+}
